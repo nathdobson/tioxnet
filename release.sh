6005d09e82a4a943109469d261e566b5264d2160
Nathan Dobson <nadobson@mac.com>
2022-12-19 15:46:06 -0800

    Fixed release.sh

--- release.sh
+++ release.sh
@@ -3,55 +3,16 @@ set -e
 set -u
 
 KIND=$1
-#OLD_VERSION="v$(npm pkg get version | cut -d \" -f 2)"
-#echo $VERSION
-#BRANCH=$(echo $VERSION | cut -d '.' -f 1-2)
-#SRC_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-#if [[ "$SRC_BRANCH" == "$BRANCH" ]]; then
-#
-#elif [["$SRC_BRANCH" == "main"]]; then
-#else
-#  "Unrecognized branch $SRC_BRANCH"
-#fi
 
 if [[ "$KIND" == "patch" ]]; then
   BRANCH="v$(npm pkg get version | cut -d \" -f 2 | cut -d '.' -f 1-2)"
 elif [[ "$KIND" == "minor" ]]; then
-  VERSION=$(npm version --no-git-tag-version minor)
+  VERSION=$(npm version minor)
   BRANCH=$(echo $VERSION | cut -d '.' -f 1-2)
-  echo $VERSION $BRANCH
-  exit
-  git add .
-  git commit -m "Bumping version to $KIND"
 else
   echo "Unknown kind $KIND"
   exit 1
 fi
-
-
-
-
-#BRANCH=v$MAJOR.$MINOR
-#TAG=v$MAJOR.$MINOR.$PATCH
-#echo "Creating branch $BRANCH with tag $TAG"
-#if [[ `git status --porcelain` ]]; then
-#  echo "There are local changes - aborting"
-#  exit 1
-#fi
-
-
-
-#if git rev-parse --verify $BRANCH > /dev/null ; then
-#  SRC_BRANCH=$BRANCH
-#  CHECKOUT="git checkout $BRANCH"
-#else
-#  CHECKOUT="git checkout -b $BRANCH"
-#fi
-
-#if [ $(git tag -l $TAG) ] ; then
-#  echo "The new tag already exists."
-#  exit 1
-#fi
 
 BUILD_DIRECTORY=/tmp/tioxnet-release-builder
 rm -rf /tmp/tioxnet-release-builder
@@ -61,6 +22,7 @@ cd $BUILD_DIRECTORY
 if [[ "$KIND" == "patch" ]]; then
   git clone -b $BRANCH file:///$ORIGIN $BUILD_DIRECTORY
   git checkout $BRANCH
+  npm version patch
 elif [[ "$KIND" == "minor" ]]; then
   git clone -b main file:///$ORIGIN $BUILD_DIRECTORY
   git checkout -b $BRANCH
@@ -69,5 +31,5 @@ fi
 echo $RANDOM > dist.txt
 git add .
 git commit -m "Built release $VERSION"
-git tag -a $VERSION -m "Releasing $VERSION"
+git tag -a release-$VERSION -m "Releasing $VERSION"
 git push --set-upstream origin $BRANCH
