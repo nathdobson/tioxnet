39f1ecdcbbc2f2586ffb50036c4497b610acdc6b
Nathan Dobson <nadobson@mac.com>
2022-12-19 14:35:11 -0800

    Fixed release.sh

--- release.sh
+++ release.sh
@@ -1,23 +1,35 @@
 #!/bin/sh
 set -e
 set -u
-VERSION=$1
-if [[ `git status --porcelain` ]]; then
-  echo "There are local changes - aborting"
-  exit 1
-fi
-if git rev-parse --verify $VERSION > /dev/null; then
-  echo "The new branch already exists."
+MAJOR=$1
+MINOR=$2
+PATCH=$3
+BRANCH=v$MAJOR.$MINOR
+TAG=v$MAJOR.$MINOR.$PATCH
+echo "Creating branch $BRANCH with tag $TAG"
+#if [[ `git status --porcelain` ]]; then
+#  echo "There are local changes - aborting"
+#  exit 1
+#fi
+#if git rev-parse --verify $BRANCH > /dev/null ; then
+#  echo "The new branch already exists."
+#  exit 1
+#fi
+
+if [ $(git tag -l $TAG) ] ; then
+  echo "The new tag already exists."
   exit 1
 fi
 
 BUILD_DIRECTORY=/tmp/tioxnet-release-builder
 rm -rf /tmp/tioxnet-release-builder
 mkdir $BUILD_DIRECTORY
-git clone --depth 1 file:///$PWD $BUILD_DIRECTORY
+git clone file:///$PWD $BUILD_DIRECTORY
 cd $BUILD_DIRECTORY
-git branch $VERSION
-./build.sh
-echo $VERSION > ./version.txt
+git checkout -b $BRANCH
+#./build.sh
+echo $TAG > ./version.txt
 git add .
-git commit -m "Built release $VERSION"
+git commit -m "Built release $TAG"
+git tag -a $TAG -m "Releasing $TAG"
+git push --set-upstream origin $BRANCH
