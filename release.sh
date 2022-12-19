7a38a18a23a740a1f753d523e041ce0d981cbb00
Nathan Dobson <nadobson@mac.com>
2022-12-19 15:18:30 -0800

    Fixed release.sh

--- release.sh
+++ release.sh
@@ -1,35 +1,60 @@
 #!/bin/sh
 set -e
 set -u
-MAJOR=$1
-MINOR=$2
-PATCH=$3
-BRANCH=v$MAJOR.$MINOR
-TAG=v$MAJOR.$MINOR.$PATCH
-echo "Creating branch $BRANCH with tag $TAG"
+
+KIND=$1
+
+if [[ "$KIND" == "patch" ]]; then
+  echo "Doing patch update."
+elif [[ "$KIND" == "minor" ]]; then
+  echo "Doing minor update."
+else
+  echo "Unknown kind $KIND"
+  exit 1
+fi
+
+VERSION=$(npm version $KIND)
+BRANCH=$(echo $VERSION | cut -d '.' -f 1-2)
+#SRC_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+#BRANCH=v$MAJOR.$MINOR
+#TAG=v$MAJOR.$MINOR.$PATCH
+#echo "Creating branch $BRANCH with tag $TAG"
 #if [[ `git status --porcelain` ]]; then
 #  echo "There are local changes - aborting"
 #  exit 1
 #fi
+
+
+
 #if git rev-parse --verify $BRANCH > /dev/null ; then
-#  echo "The new branch already exists."
-#  exit 1
+#  SRC_BRANCH=$BRANCH
+#  CHECKOUT="git checkout $BRANCH"
+#else
+#  CHECKOUT="git checkout -b $BRANCH"
 #fi
 
-if [ $(git tag -l $TAG) ] ; then
-  echo "The new tag already exists."
-  exit 1
-fi
+#if [ $(git tag -l $TAG) ] ; then
+#  echo "The new tag already exists."
+#  exit 1
+#fi
 
 BUILD_DIRECTORY=/tmp/tioxnet-release-builder
 rm -rf /tmp/tioxnet-release-builder
 mkdir $BUILD_DIRECTORY
-git clone file:///$PWD $BUILD_DIRECTORY
+ORIGIN=$PWD
 cd $BUILD_DIRECTORY
-git checkout -b $BRANCH
+if [[ "$KIND" == "patch" ]]; then
+  git clone $BRANCH file:///$ORIGIN $BUILD_DIRECTORY
+  git checkout $BRANCH
+elif [[ "$KIND" == "minor" ]]; then
+  git clone main file:///ORIGIN $BUILD_DIRECTORY
+  git checkout -b $BRANCH
+fi
 #./build.sh
-echo $TAG > ./version.txt
+echo $RANDOM > dist.txt
 git add .
-git commit -m "Built release $TAG"
-git tag -a $TAG -m "Releasing $TAG"
+git commit -m "Built release $VERSION"
+git tag -a $VERSION -m "Releasing $VERSION"
 git push --set-upstream origin $BRANCH
+git push --set-upstream origin $VERSION
