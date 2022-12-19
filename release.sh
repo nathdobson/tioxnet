cbb2a7327d4f92a41cd6e1125cff77acbe15d426
Nathan Dobson <nadobson@mac.com>
2022-12-19 15:52:47 -0800

    Fixed release.sh

--- release.sh
+++ release.sh
@@ -5,31 +5,35 @@ set -u
 KIND=$1
 
 if [[ "$KIND" == "patch" ]]; then
-  BRANCH="v$(npm pkg get version | cut -d \" -f 2 | cut -d '.' -f 1-2)"
+
 elif [[ "$KIND" == "minor" ]]; then
-  VERSION=$(npm version minor)
-  BRANCH=$(echo $VERSION | cut -d '.' -f 1-2)
+
 else
   echo "Unknown kind $KIND"
   exit 1
 fi
+
+SRC_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 BUILD_DIRECTORY=/tmp/tioxnet-release-builder
 rm -rf /tmp/tioxnet-release-builder
 mkdir $BUILD_DIRECTORY
 ORIGIN=$PWD
 cd $BUILD_DIRECTORY
+git clone -b $SRC_BRANCH file:///$ORIGIN $BUILD_DIRECTORY
 if [[ "$KIND" == "patch" ]]; then
-  git clone -b $BRANCH file:///$ORIGIN $BUILD_DIRECTORY
   git checkout $BRANCH
-  npm version patch
+  VERSION=$(npm version --no-git-tag-version patch)
 elif [[ "$KIND" == "minor" ]]; then
   git clone -b main file:///$ORIGIN $BUILD_DIRECTORY
   git checkout -b $BRANCH
+  VERSION=$(npm version --no-git-tag-version  minor)
 fi
+BRANCH=$(echo $VERSION | cut -d '.' -f 1-2)
+
 #./build.sh
 echo $RANDOM > dist.txt
 git add .
 git commit -m "Built release $VERSION"
 git tag -a release-$VERSION -m "Releasing $VERSION"
-git push --set-upstream origin $BRANCH
+#git push --set-upstream origin $BRANCH
